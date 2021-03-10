28bae9f2de67ab4bde24e35ccb3500e18bb54603
Newton Zou <newton.zou@aveva.com>
2021-03-10 13:31:32 +0800

    update

--- GenA3Data.ps1
+++ GenA3Data.ps1
@@ -6,13 +6,11 @@ param(
 $membersCalc = @{ }
 $calcHours = @{ }
 
-
 $TableData = Import-Csv -Path $csvFile
 $lastStoryPoints = [double]0
 $lastStoryTotalHours = [double]0
 foreach ($DataValue in $TableData) {
     if (-not [string]::IsNullOrEmpty($DataValue.'Story Points')) {
-        #if ($DataValue.'Work Item Type' -eq 'User Story' -or  $DataValue.'Work Item Type' -eq 'Bug')
         ##Calculate last one
         if ($lastStoryPoints -ne 0 -and $lastStoryTotalHours -ne 0) {
             $teamMembers | ForEach-Object { $membersCalc[$_] += $lastStoryPoints * $calcHours[$_] / $lastStoryTotalHours }
@@ -28,10 +26,9 @@ foreach ($DataValue in $TableData) {
             $calcHours[$DataValue.'Assigned To'] += [double]($DataValue.'Completed Work')
         }
     }
-
 }
 
-if ($lastStoryPoints -ne 0) {
+if ($lastStoryPoints -ne 0 -and $lastStoryTotalHours -ne 0) {
     $teamMembers | ForEach-Object { $membersCalc[$_] += $lastStoryPoints * $calcHours[$_] / $lastStoryTotalHours }
 }
 
